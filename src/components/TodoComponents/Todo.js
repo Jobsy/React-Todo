1b383ee44ac44cad4be0d351fdbfb1e58ba66561
Oluwajoba <oluwajobabello@gmail.com>
2019-10-01 03:35:20 +0100

    add data / Todo.js

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -3,16 +3,43 @@ import { directive } from "@babel/types";
 
 
 class Todo extends React.Component {
-    constructor() {
-        super();
-        this.state = {};
-    }
+  constructor() {
+    super();
+    this.state = {
+      data: [
+        {
+          id: "1",
+          title: "Buy Milk.",
+          done: false,
+          date: new Date()
+        },
+        {
+          id: "2",
+          title: "Meeting with Ali.",
+          done: false,
+          date: new Date()
+        },
+        {
+          id: "3",
+          title: "Tea break.",
+          done: false,
+          date: new Date()
+        },
+        {
+          id: "4",
+          title: "Go for a run.",
+          done: false,
+          date: new Date()
+        }
+      ]
+    };
+  }
 
-    render() {
-        return(
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
 }
