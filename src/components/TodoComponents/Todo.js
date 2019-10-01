8c0e49cd425bc21a465bad46ced1552eb8e73643
Oluwajoba <oluwajobabello@gmail.com>
2019-10-01 03:30:49 +0100

    add class Todo / Todo.js

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,18 @@
+import react from "react";
+import { directive } from "@babel/types";
+
+
+class Todo extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
